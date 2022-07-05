b3712e7f1c0a5f228cfb9d288e895c956b76703b
yuvron <yuvr99@gmail.com>
2022-07-05 17:19:59 +0300

    Created Cookie Manager

---
+++ src/client/scripts/cookiesManager.ts
@@ -0,0 +1,29 @@
+export class CookiesManager {
+	static addCookie(name: string, value: string): void {
+		document.cookie = `${name}=${value}`;
+	}
+
+	static getCookie(name: string): Cookie | undefined {
+		const cookies = document.cookie.split(";");
+		const matchingCookie = cookies.find((cookie) => cookie.trim().split(";")[0] === name);
+		return matchingCookie ? { name: matchingCookie[0], value: matchingCookie[1] } : undefined;
+	}
+
+	static deleteCookie(name: string): void {
+		document.cookie = `${name}=a;expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+	}
+
+	static deleteAllCookies(): void {
+		const cookies = document.cookie.split(";");
+		cookies.forEach((cookie) => {
+			cookie = cookie.trim();
+			const cookieName = cookie.split("=")[0];
+			CookiesManager.deleteCookie(cookieName);
+		});
+	}
+}
+
+export interface Cookie {
+	name: string;
+	value: string;
+}
